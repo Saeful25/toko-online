bb078db2547cc4b214ce960f5bb146e8d28b606e
Saeful25 <msaeful858@gmail.com>
2024-03-27 07:38:55 +0700

    tugas fronend

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,9 @@ import Product from "./Products.js";
 const Main = () => {
     return (
         <>
-        <Product nama="Kursi Gaming Fantech" harga={2.350}  gambar="https://cdn-icons-png.flaticon.com/512/6997/6997668.png"></Product>
-      <Product nama="Poco X6 Pro" harga={4.999} gambar="https://cdn-icons-png.flaticon.com/512/6997/6997668.png"></Product>
-      <Product nama="SSD Seagate 1 TB Gen 4" harga={2.450} gambar="https://cdn-icons-png.flaticon.com/512/6997/6997668.png"></Product>
+        <Product nama="Kursi Gaming Fantech" harga={2.350}  gambar="https://asterindoshop.com/gambar/produk/large-kursi-gaming-fantech-chair-alpha-gc-182-191221040522-47.jpg"></Product>
+      <Product nama="Poco X6 Pro" harga={4.999} gambar="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/95/MTA-155504090/br-m036969-03591_poco-x6-pro_full01-a6020a2c.jpg"></Product>
+      <Product nama="SSD Seagate 1 TB Gen 4" harga={2.450} gambar="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/10/25/299501ce-f220-487d-addc-a0c2f925a2e2.jpg"></Product>
         </>
     )
 }
